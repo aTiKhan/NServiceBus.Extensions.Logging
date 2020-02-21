f2cf74597afbcf98ce9250281309e67709d6c448
Ramon Smits <ramon.smits@gmail.com>
2020-02-21 10:49:00 +0100

    Import of current work.

---
+++ src/NServiceBus.Extensions.Logging/ExtensionsLoggerFactory.cs
@@ -0,0 +1,36 @@
+namespace NServiceBus.Extensions.Logging
+{
+    using System;
+    using NServiceBus.Logging;
+
+    /// <summary>
+    /// Usage:
+    ///       ILoggerFactory extensionsLoggingFactory = ...;
+    ///       LogManager.UseFactory(new ExtensionsLoggerFactory(extensionsLoggingFactory));
+    /// </summary>
+    public class ExtensionsLoggerFactory : ILoggerFactory
+    {
+        /// <summary>
+        /// Create a NServiceBus `ILoggerFactory` instance based on `Microsoft.Extensions.Logging`.
+        /// </summary>
+        /// <param name="loggerFactory">An initialized <see cref="Microsoft.Extensions.Logging.ILoggerFactory"/> instance.</param>
+        public ExtensionsLoggerFactory(Microsoft.Extensions.Logging.ILoggerFactory loggerFactory)
+        {
+            this.loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
+        }
+
+        /// <inheritdoc />
+        public ILog GetLogger(Type type)
+        {
+            return new ExtensionsLogger(loggerFactory.CreateLogger(type.FullName));
+        }
+
+        /// <inheritdoc />
+        public ILog GetLogger(string name)
+        {
+            return new ExtensionsLogger(loggerFactory.CreateLogger(name));
+        }
+
+        Microsoft.Extensions.Logging.ILoggerFactory loggerFactory;
+    }
+}
