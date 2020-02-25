1187f951c0fbc360254ef41635bd54ac3f05f3c5
Hadi Eskandari <h.eskandari@gmail.com>
2020-02-25 11:34:14 +1100

    added approval tests

---
+++ src/NServiceBus.Extensions.Logging.Tests/APIApprovals.cs
@@ -0,0 +1,23 @@
+﻿namespace NServiceBus.Extensions.Logging.Tests
+{
+    using Logging;
+    using NUnit.Framework;
+    using Particular.Approvals;
+    using PublicApiGenerator;
+
+    [TestFixture]
+    public class APIApprovals
+    {
+        [Test]
+        public void Approve_NServiceBusExtensionsLogging()
+        {
+            var options = new ApiGeneratorOptions
+            {
+                ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" }
+            };
+
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(ExtensionsLoggerFactory).Assembly, options);
+            Approver.Verify(publicApi);
+        }
+    }
+}
