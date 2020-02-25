93d371a391284e21050c36b274b033a17a04c413
Hadi Eskandari <h.eskandari@gmail.com>
2020-02-25 12:27:23 +1100

    added acceptance tests for nlog

---
+++ src/NServiceBus.Extensions.Logging.NLog.AcceptanceTests/When_using_nlog_extension_logger.cs
@@ -0,0 +1,42 @@
+﻿namespace NServiceBus.Extensions.DependencyInjection.AcceptanceTests
+{
+    using System.Threading.Tasks;
+    using Logging;
+    using NLog;
+    using NLog.Extensions.Logging;
+    using NUnit.Framework;
+    using NsbLogManager = NServiceBus.Logging.LogManager;
+
+    public class When_using_nlog_extension_logger
+    {
+        [Test]
+        public async Task Should_log_nsb_logs()
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+            var memoryTarget = new NLog.Targets.MemoryTarget();
+            config.AddRuleForAllLevels(memoryTarget);
+            LogManager.Configuration = config;
+            
+            NsbLogManager.UseFactory(new ExtensionsLoggerFactory(new NLogLoggerFactory()));
+            
+            var endpointConfiguration = new EndpointConfiguration("LoggingTests");
+            endpointConfiguration.EnableInstallers();
+            endpointConfiguration.SendFailedMessagesTo("error");
+            endpointConfiguration.UseTransport<LearningTransport>();
+            endpointConfiguration.UsePersistence<InMemoryPersistence>();
+            
+            var endpoint = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+            
+            try
+            {
+                Assert.IsNotEmpty(memoryTarget.Logs);
+            }
+            finally
+            {
+                await endpoint.Stop()
+                    .ConfigureAwait(false);
+            }
+        }
+    }
+}
